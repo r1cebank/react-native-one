93cc3f233addd5d8f2d37c6f6b643822f16ea638
r1cebank <siyuangao@gmail.com>
2017-12-21 00:18:30 -0800

    adding storage

---
+++ src/storage/example.js
@@ -0,0 +1,10 @@
+/**
+ * Example storage, make requests to server, cache if needed
+ */
+export function get () {
+    // Make some network request
+}
+
+export function set () {
+    // Make some network request
+}
